8fbc945bc5e695915a63341241e5032679c62c7e
Kajetan Wiśniewski <k.wisniewski.603@studms.ug.edu.pl>
2023-11-15 16:30:12 +0100

    feature13+test w parameterized

---
+++ app/tests/test_parameterized.py
@@ -0,0 +1,35 @@
+import unittest
+
+from ..Account import Account
+from ..CustomerAccount import CustomerAccount
+from ..CompanyAccount import CompanyAccount
+
+from parameterized import parameterized
+
+class TestCredit(unittest.TestCase):
+
+    companyName = "UG"
+    NIP = "8904567890"
+
+    # @parameterized.expand([
+    # ([100, 100, 100], 500, True, 500),
+    # ([100, 100, 100], -200, 500, False, 0),])
+
+    # def test_loan_take_out(self, history, loan_amount, expected_output, expected_saldo):
+    #     account = CustomerAccount(self.imie, self.nazwisko, self.pesel)
+    #     account.history = historyis_loan = account.loan(loan_amount)
+    #     self.assertEqual(account.balance, expected_output)
+
+    @parameterized.expand([
+    ([100, 100, 1775], 5000, 1200, True, 6200),
+    ([100, 100, 100], 500, 900, False, 500),
+    ])
+
+    
+    def test_loan_take_out(self, history, balance, loan_amount, expected_output, expected_balance):
+        account = CompanyAccount(self.companyName, self.NIP)
+        account.balance = balance
+        account.history = history
+        is_loan = account.get_credit(loan_amount, history)
+        self.assertEqual(account.balance, expected_balance)
+        self.assertEqual(is_loan, expected_output)
