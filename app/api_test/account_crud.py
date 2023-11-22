20df980673b71ebddb5fecf1fae085b9b15fca6f
Kajetan Wiśniewski <k.wisniewski.603@studms.ug.edu.pl>
2023-11-22 17:30:33 +0100

    first tests

--- app/api_test/account_crud.py
+++ app/api_test/account_crud.py
@@ -6,3 +6,9 @@ class testAccountCrud(unittest.TestCase):
     def setUp(self):
         self.url = "http://localhost:5000/api/accounts"
     
+    post_data = {"imie": "Jacek", "nazwisko": "jaworek", "pesel": "89045678902"}
+    #to ma byc json zeby dzialalo
+
+    def testPostCreatAcc(self):
+        r = requests.post(self.url, data = self.post_data)
+        self.assertEqual(r.status_code, 201)
